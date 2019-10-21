0d00f50ad4e111d87adfe47f85cd51c4fdfc8f02
Viktor Parahonya <stelsvitya@gmail.com>
2019-10-21 12:08:59 +0300

    write homeAction to check for some content

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -5,9 +5,16 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testHomePage() {
+    public function testHomePageIsAvailable() {
         $client = static::createClient();
         $client->request('GET', '/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    public function testHomePageContainsSomeContent() {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertContains('Very Secured data', $client->getResponse()->getContent());
+    }
+
 }
