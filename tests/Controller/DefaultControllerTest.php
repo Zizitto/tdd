6bc3901c1f522ec9d6da996788d48df2a5ef76d2
Viktor Parahonya <stelsvitya@gmail.com>
2019-10-21 13:51:33 +0300

    fix test to check login

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,19 +2,52 @@
 namespace tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\BrowserKit\Cookie;
 
 class DefaultControllerTest extends WebTestCase
 {
+    private $client = null;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
     public function testHomePageIsAvailable() {
-        $client = static::createClient();
-        $client->request('GET', '/');
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->client->request('GET', '/');
+        $this->assertEquals(401, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testHomePageIsAvailableWithGoodCredentials() {
+        $this->login();
+        $this->client->request('GET', '/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
     public function testHomePageContainsSomeContent() {
-        $client = static::createClient();
-        $client->request('GET', '/');
-        $this->assertContains('Very Secured data', $client->getResponse()->getContent());
+        $this->login();
+        $this->client->request('GET', '/');
+        $this->assertContains('Very Secured data', $this->client->getResponse()->getContent());
+    }
+
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewallName = 'main';
+        // if you don't define multiple connected firewalls, the context defaults to the firewall name
+        // See https://symfony.com/doc/current/reference/configuration/security.html#firewall-context
+        $firewallContext = 'main';
+
+        // you may need to use a different token class depending on your application.
+        // for example, when using Guard authentication you must instantiate PostAuthenticationGuardToken
+        $token = new UsernamePasswordToken('admin', null, $firewallName, ['ROLE_ADMIN']);
+        $session->set('_security_'.$firewallContext, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
     }
 
 }
